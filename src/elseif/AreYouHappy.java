058db5146a8d2f5210c93dd92f32c86bf91000e8
league <league@wts-im-17.attlocal.net>
2018-12-01 14:30:33 -0800

    Module 3

---
+++ src/elseif/AreYouHappy.java
@@ -0,0 +1,17 @@
+package elseif;
+
+import javax.swing.JOptionPane;
+
+public class AreYouHappy {
+public static void main(String[] args) {
+	String happy = JOptionPane.showInputDialog("Are you happy?");
+	if (happy.equalsIgnoreCase("Yes")) {
+		JOptionPane.showMessageDialog(null, "Keep doing whatever your're doing.");
+	}
+	else {
+	String doWantHappy = JOptionPane.showInputDialog(null, "Do you want to be happy?");
+		if 
+		
+	}
+}
+}
--- src/elseif/BirthdayReminder.java
+++ src/elseif/BirthdayReminder.java
@@ -11,15 +11,27 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "November 4th";
+		String dadsBirthday = "December 7th";
+		String myBirthday = "July 8th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		String birthday = JOptionPane.showInputDialog("Who's birthday do you want? My mom's birthday, my dad's birthday, or my birthday? Type in Mom, Dad, or my birthday. Caps don't matter.");
 		// 3. Print out what the user typed
 		
 		// 4. if user asked for "mom"
+		if (birthday.equalsIgnoreCase("Mom")) {
+			JOptionPane.showMessageDialog(null, momsBirthday);
+		}
+		else if (birthday.equalsIgnoreCase("Dad")) {
+			JOptionPane.showMessageDialog(null, dadsBirthday);
+		}
+		else if (birthday.equalsIgnoreCase("My Birthday")) {
+			JOptionPane.showMessageDialog(null, myBirthday);
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Sorry, I don't remember that person's birthday.");
+		}
 			//print mom's birthday
 		// 5. if user asked for "dad"
 			// print dad's birthday
