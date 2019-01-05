1949cc087721ac50b8d35f2c18a0aabb1d446201
league <league@imac-6.attlocal.net>
2019-01-05 13:30:40 -0800

    Spud counter checkpoint.

---
+++ src/extra/SpudCounter.java
@@ -0,0 +1,20 @@
+package extra;
+
+public class SpudCounter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for (int i = 1; i < 9; i++) {
+	if (i==4) {
+		System.out.println("4");
+	}
+	else if (i==8) {
+		System.out.println("more");
+	}
+	else {
+		System.out.println(i+" potato,");
+	}
+}
+	}
+
+}
